ab541c111b8e145e9a599ff35e4064a287a22d2e
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-04-04 16:54:46 +0800

    Update pointnet2_cls_ssg.py

--- models/pointnet2_cls_ssg.py
+++ models/pointnet2_cls_ssg.py
@@ -13,10 +13,8 @@ import tf_util
 from pointnet_util import pointnet_sa_module
 
 def placeholder_inputs(batch_size, num_point):
-    #pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
-    #labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
-    pointclouds_pl = tf.compat.v1.placeholder(tf.float32, shape=(batch_size, num_point, 3))
-    labels_pl = tf.compat.v1.placeholder(tf.int32, shape=(batch_size))
+    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
+    labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
     return pointclouds_pl, labels_pl
 
 def get_model(point_cloud, is_training, bn_decay=None):
