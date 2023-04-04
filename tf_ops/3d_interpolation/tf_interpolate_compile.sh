190d227d32d94522f22e3fc8e8e45c211ff39a36
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-04-04 15:30:20 +0800

    Update tf_interpolate_compile.sh

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+TF_DIR=$(dirname $(python -c "import tensorflow as tf;print(tf.__file__)"))
+CUDA_HOME=/usr/local/cuda-11
+
 # TF1.2
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I ${TF_DIR}/include -I ${CUDA_HOME}/include -lcudart -L ${CUDA_HOME}/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
 # TF1.4
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I ${TF_DIR}/include -I /usr/local/cuda-8.0/include -I ${TF_DIR}/include/external/nsync/public -lcudart -L ${CUDA_HOME}/lib64/ -L${TF_DIR} -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
